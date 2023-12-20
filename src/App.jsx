359037d33ce77c4b586b0d3cb31f8e4294536354
Anastasia <ohnnohnn@yandex.ru>
2023-12-20 12:05:01 +0300

    fix: обработка ошибки api response

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,14 @@ function App() {
       const response =  await fetch(
         `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=auto:ip&lang=ru&days=3`
       );
+      
       const data = await response.json();
+      if (data.error) {
+        throw new Error(data.error)
+        /* setHint(`Ошибка при получении данных. Код ошибки: ${data.error.code}`)
+        setLoading(false)
+        return */
+      }
       setWeatherData({
         city: data.location.name,
         is_day: data.current.is_day,
@@ -42,8 +49,9 @@ function App() {
       
       setLoading(false);
     } catch (e) {
+      console.log(e)
       setLoading(false);
-      setHint(/* HINT_MESSAGES.error */e.message);
+      setHint(`Произошла ошибка при получении данных: ${e.message}`);
     }
   }
 
